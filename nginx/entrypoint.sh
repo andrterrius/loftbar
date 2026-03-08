f46cba0f1852cc6ba58516f73a238f53e3bc4640
andrterrius <chashkin.andreei@mail.ru>
2026-03-08 23:18:36 +0300

    fix: automatic receipt and renewal of SSL certificates

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,11 +3,16 @@
 if [ "$PRODUCTION" = "True" ]; then
     echo "Production mode: using only APP_DOMAIN"
     export SERVER_NAMES="${APP_DOMAIN}"
+
+    (while :; do sleep 8h; nginx -s reload; done) &
+
 else
     echo "Development mode: using localhost and APP_DOMAIN"
     export SERVER_NAMES="localhost ${APP_DOMAIN}"
+
+    (while :; do sleep 4h; nginx -s reload; done) &
 fi
 
 envsubst '${BACKEND_PORT} ${FRONTEND_PORT} ${APP_DOMAIN} ${SERVER_NAMES}' < /etc/nginx/conf.d/template > /etc/nginx/conf.d/default.conf
 
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
