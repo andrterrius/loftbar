0f40618ed3ece15f928c23f2dafcb4503343d200
Ilja <ilja_anisimov@MacBook-Air-Ilja.local>
2026-03-27 16:01:15 +0300

    fix: delay page mount until auth token is ready

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 
 
 export default function RootLayout({ children }) {
+  
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
@@ -24,7 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <InitTelegramAuth/>
-        {children}
+        <AuthGate>{children}</AuthGate>
       </body>
     </html>
   );
