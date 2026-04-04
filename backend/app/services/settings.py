a4bae209a5034429400db349ee549f05c0cc4b78
andrterrius <chashkin.andreei@mail.ru>
2026-04-04 21:07:27 +0300

    fix: preset_base_price in settings service

--- backend/app/services/settings.py
+++ backend/app/services/settings.py
@@ -9,7 +9,10 @@ class SettingsService(BaseSettingsService):
     async def get_preset_base_price(self, uow: BaseUnitOfWork) -> PresetBasePrice:
         async with uow:
             settings = await uow.settings.get_by_id(1)
-            return PresetBasePrice.model_validate(settings)
+            return PresetBasePrice(
+                base_price=settings.preset_base_price,
+                strength_added_price=settings.strength_added_price
+            )
 
     async def get_images(self, uow: BaseUnitOfWork) -> SettingsImagesOut:
         async with uow:
